53a60fefdda75309d75e4ebc37c6149797e86361
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2016-10-15 14:01:11 +0100

    Added right project layout

---
+++ DADSTORM/FileManipulator/EOFException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManipulator {
+    public class EOFException : System.ApplicationException {
+        public EOFException() { }
+
+        public EOFException(string message) : base(message) { }
+
+        public EOFException(string message, System.Exception inner) : base(message, inner) { }
+    }
+}
---
+++ DADSTORM/FileManipulator/LineParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManipulator {
+    public class LineParser {
+        private string line;
+        private ReadOnlyCollection<string> words;
+
+        public LineParser(string line) {
+            if (line == null || line.Equals(""))
+                throw new EmptyLineException();
+
+            this.line = line;
+        }
+
+        public ReadOnlyCollection<string> Words {
+            get {
+                if (words != null)
+                    return words;
+
+                char[] delimiters = { ' ', ',', '\t' };
+                return words = new ReadOnlyCollection<string>(line.Split(delimiters, System.StringSplitOptions.RemoveEmptyEntries));
+            }
+        }
+    }
+}
