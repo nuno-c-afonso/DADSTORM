313b4c18e9b01fb845368101e62e54a2f8d3eed6
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2016-10-17 18:00:46 +0100

    Added some command files

---
+++ DADSTORM/CommandLine/Shell.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLine
+{
+    public class Shell {
+        private Dictionary<string, Command> commands;
+
+        public Shell() {
+            init();
+        }
+
+        public Shell(Dictionary<string, Command> newCommands) {
+            init();
+            commands = commands.Concat(newCommands).ToDictionary(k => k.Key, v => v.Value);
+        }
+
+        private void init() {
+            commands.Add("start", new StartCommand());
+            commands.Add("interval", new IntervalCommand());
+            commands.Add("status", new StatusCommand());
+            commands.Add("crash", new CrashCommand());
+            commands.Add("freeze", new FreezeCommand());
+            commands.Add("unfreeze", new UnfreezeCommand());
+        }
+
+        public void execute() {
+            bool leave = false;
+            string prompt = "> ";
+            string line;
+
+            Console.WriteLine("Puppet Master Shell ('exit' to leave)");
+            Console.WriteLine(prompt);
+
+            while (!leave) {
+                char[] delimiters = { ' ' };
+                line = Console.ReadLine();
+
+                string[] arg = line.Split(delimiters, System.StringSplitOptions.RemoveEmptyEntries);
+                Command c = commands[arg[0]];
+
+                // TODO: Finish the class
+            }
+        }
+    }
+}
