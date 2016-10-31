2f260532ee68ed6af2712331e10cc4d8a896a707
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2016-10-31 17:57:07 +0000

    Added processed tuples to Operation class

--- DADSTORM/Replica/Operation.cs
+++ DADSTORM/Replica/Operation.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace Replica{
     public abstract class Operation {
+        private HashSet<string> seenStrings = new HashSet<string>();
 
         public abstract List<string[]> Operate(string[] tuple);
             
+        protected void processedTuple(string[] tuple) {
+            foreach (string s in tuple)
+                seenStrings.Add(s);
+        }
 
+        protected bool wasElementSeen(string s) {
+            return seenStrings.Contains(s);
+        }
     }
 }
 
--- DADSTORM/Replica/UniqOperation.cs
+++ DADSTORM/Replica/UniqOperation.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Replica
-{
-    public class UniqOperation : Operation
-    {
-        public override List<string[]> Operate(string[] tuple)
-        {
+namespace Replica {
+    public class UniqOperation : Operation {
+        public override List<string[]> Operate(string[] tuple) {
             //TODO implement
             throw new NotImplementedException();
         }
