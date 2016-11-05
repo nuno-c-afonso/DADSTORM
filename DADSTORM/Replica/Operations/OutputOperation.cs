149912c84814fbeebb2cee3f1fd1a01022f4a2ed
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2016-11-05 11:37:06 +0000

    Added teacher's OutputOperation

---
+++ DADSTORM/Replica/Operations/OutputOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replica {
+    public class OutputOperation : Operation {
+        public override List<string[]> Operate(string[] tuple) {
+            string outputFile = @".\output.txt";
+
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(outputFile, true)) {
+                foreach (string line in tuple) {
+
+                    // If the line doesn't contain the word 'Second', write the line to the file.
+                    file.WriteLine(line);
+                }
+            }
+
+            List<string[]> result = new List<string[]>();
+
+            result.Add(tuple);
+            return result;
+        }
+    }
+}
