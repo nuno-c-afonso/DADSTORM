08b0a480779c0aefe37712b2b03f31d5555709eb
Diogo Silva <dyx_silva@hotmail.com>
2016-11-05 16:49:46 +0000

    created a function to feed the buffer

---
+++ DADSTORM/Replica/tupleFileReader.cs
@@ -0,0 +1,40 @@
+﻿using CommonClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replica
+{
+    public class tupleFileReader
+    {
+        private ReplicaInterface buffer;
+        private string[] lines;
+
+        public tupleFileReader(ReplicaInterface buffer, string filepath)
+        {
+            this.buffer = buffer;
+            lines = System.IO.File.ReadAllLines(filepath);
+            lines = lines.Where(line => (line.Length > 0 && line[0] != '%')).ToArray();
+        } 
+
+        public void feedBuffer(){
+            string[] tuple;
+            foreach(string line in lines){
+                tuple = getTupleFromLine(line);
+                buffer.addTuple(tuple);
+            }
+
+        }
+
+        public string[] getTupleFromLine(string line){
+            string[] delimiters = { ", ", " " };
+            string[] tuple = line.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            return tuple;
+        }
+
+
+
+    }
+}
