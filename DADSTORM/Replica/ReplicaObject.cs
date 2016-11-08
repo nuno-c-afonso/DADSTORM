e2e52266233c5292a114b7d011716c3ce1afd045
Diogo Silva <dyx_silva@hotmail.com>
2016-11-08 16:03:28 +0000

    finished file reader close #24

--- DADSTORM/Replica/ReplicaObject.cs
+++ DADSTORM/Replica/ReplicaObject.cs
@@ -21,10 +21,13 @@ namespace Replica {
         private string replicaAddress;
         private string operationName;
         
-        bool start = false;
-        int waitingTime = 0;
-        bool crashed = false;
-        bool freezed = false;
+        private bool start = false;
+        private int waitingTime = 0;
+        private bool crashed = false;
+        private bool freezed = false;
+        public bool Started {
+            get { return start; }
+        }
 
         public ReplicaObject(string PuppetMasterUrl, string routing, string semantics,
             string logLevel, Operation operation, List<string> output, string replicaAddress, string operationName) {
