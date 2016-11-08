ac2fd38f897fc7a688a70aa333e59cf2f77f27d8
Ricardo Mota <ricardo.r.mota@tecnico.ulisboa.pt>
2016-11-08 00:36:56 +0000

    Fixed resize of GUI windows, Added Tuple sent log

--- DADSTORM/PuppetMasterGUI/Form1.Designer.cs
+++ DADSTORM/PuppetMasterGUI/Form1.Designer.cs
@@ -43,10 +43,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(241, 49);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(181, 40);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 23);
+            this.button1.Size = new System.Drawing.Size(111, 19);
             this.button1.TabIndex = 0;
             this.button1.Text = "Run one command";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,10 +54,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(396, 49);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(297, 40);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 23);
+            this.button2.Size = new System.Drawing.Size(111, 19);
             this.button2.TabIndex = 1;
             this.button2.Text = "Run all commands";
             this.button2.UseVisualStyleBackColor = true;
@@ -66,68 +66,76 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 14);
+            this.label1.Location = new System.Drawing.Point(7, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 17);
+            this.label1.Size = new System.Drawing.Size(88, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Configuration file:";
             // 
             // ConsoleBox
             // 
-            this.ConsoleBox.Location = new System.Drawing.Point(556, 79);
-            this.ConsoleBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ConsoleBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ConsoleBox.Location = new System.Drawing.Point(417, 64);
+            this.ConsoleBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ConsoleBox.Multiline = true;
             this.ConsoleBox.Name = "ConsoleBox";
             this.ConsoleBox.ReadOnly = true;
             this.ConsoleBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.ConsoleBox.Size = new System.Drawing.Size(429, 267);
+            this.ConsoleBox.Size = new System.Drawing.Size(516, 219);
             this.ConsoleBox.TabIndex = 4;
             // 
             // Output
             // 
             this.Output.AutoSize = true;
-            this.Output.Location = new System.Drawing.Point(555, 54);
+            this.Output.Location = new System.Drawing.Point(416, 44);
+            this.Output.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Output.Name = "Output";
-            this.Output.Size = new System.Drawing.Size(32, 17);
+            this.Output.Size = new System.Drawing.Size(25, 13);
             this.Output.TabIndex = 5;
             this.Output.Text = "Log";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 53);
+            this.label3.Location = new System.Drawing.Point(7, 43);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(163, 17);
+            this.label3.Size = new System.Drawing.Size(122, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Previously run command";
             // 
             // textBox2
             // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.textBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.textBox2.Location = new System.Drawing.Point(11, 160);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Location = new System.Drawing.Point(8, 116);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(532, 186);
+            this.textBox2.Size = new System.Drawing.Size(400, 167);
             this.textBox2.TabIndex = 7;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(11, 79);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Location = new System.Drawing.Point(8, 64);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(532, 54);
+            this.textBox1.Size = new System.Drawing.Size(400, 31);
             this.textBox1.TabIndex = 8;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 139);
+            this.label2.Location = new System.Drawing.Point(7, 99);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(108, 17);
+            this.label2.Size = new System.Drawing.Size(83, 13);
             this.label2.TabIndex = 9;
             this.label2.Text = "Next commands";
             // 
@@ -137,10 +145,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(276, 8);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(207, 6);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 25);
+            this.button3.Size = new System.Drawing.Size(75, 20);
             this.button3.TabIndex = 10;
             this.button3.Text = "Open file...";
             this.button3.UseVisualStyleBackColor = true;
@@ -148,35 +155,33 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(135, 9);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Location = new System.Drawing.Point(101, 7);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 11;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(553, 14);
+            this.label4.Location = new System.Drawing.Point(415, 11);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(70, 17);
+            this.label4.Size = new System.Drawing.Size(53, 13);
             this.label4.TabIndex = 12;
             this.label4.Text = "Script file:";
             // 
             // scriptTextBox
             // 
-            this.scriptTextBox.Location = new System.Drawing.Point(630, 11);
-            this.scriptTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.scriptTextBox.Location = new System.Drawing.Point(472, 9);
             this.scriptTextBox.Name = "scriptTextBox";
-            this.scriptTextBox.Size = new System.Drawing.Size(132, 22);
+            this.scriptTextBox.Size = new System.Drawing.Size(100, 20);
             this.scriptTextBox.TabIndex = 13;
             // 
             // scriptButton
             // 
-            this.scriptButton.Location = new System.Drawing.Point(770, 8);
-            this.scriptButton.Margin = new System.Windows.Forms.Padding(4);
+            this.scriptButton.Location = new System.Drawing.Point(578, 6);
             this.scriptButton.Name = "scriptButton";
-            this.scriptButton.Size = new System.Drawing.Size(100, 27);
+            this.scriptButton.Size = new System.Drawing.Size(75, 22);
             this.scriptButton.TabIndex = 14;
             this.scriptButton.Text = "Open file...";
             this.scriptButton.UseVisualStyleBackColor = true;
@@ -184,9 +189,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(998, 358);
+            this.ClientSize = new System.Drawing.Size(944, 298);
             this.Controls.Add(this.scriptButton);
             this.Controls.Add(this.scriptTextBox);
             this.Controls.Add(this.label4);
@@ -201,7 +206,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Puppet Master";
             this.Load += new System.EventHandler(this.FormPuppetMaster_Load);
