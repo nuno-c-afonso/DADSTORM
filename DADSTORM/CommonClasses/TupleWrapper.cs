6f9bbd414392d29807436ebd4271f52613fb8d0a
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2016-12-05 16:44:09 +0000

    Started the == operator on TuppleWrapper

--- DADSTORM/CommonClasses/TupleWrapper.cs
+++ DADSTORM/CommonClasses/TupleWrapper.cs
@@ -10,6 +10,26 @@ namespace CommonClasses {
         private int[] id;
         private string[] tuple;
 
+        // TODO: Include the replica's index in the ID
+        public static bool operator ==(TupleWrapper t1, TupleWrapper t2) {
+            int size = t1.ID.Length;
+
+            if (size == t2.ID.Length) {
+                for (int i = 0; i < size; i++) {
+                    if (t1.ID[i] != t2.ID[i])
+                        return false;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(TupleWrapper t1, TupleWrapper t2) {
+            return !(t1 == t2);
+        }
+
         public int[] ID {
             get {
                 return id;
