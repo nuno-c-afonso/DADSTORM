7b7c5bd48b8c3c214690e20f1812be342f89c5e3
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2016-12-05 13:56:13 +0000

    Created TupleWrapper class for the exactly-once semantics

---
+++ DADSTORM/Replica/TupleWrapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replica {
+    public class TupleWrapper {
+        private int[] id;
+        private string[] tuple;
+
+        public int[] ID {
+            get {
+                return ID;
+            }
+        }
+
+        public string[] Tuple {
+            get {
+                return tuple;
+            }
+        }
+
+        public TupleWrapper(int[] previousID, int replicaID, string[] tuple) {
+            int previousSize = previousID.Length;
+            id = new int[previousSize + 1];
+            previousID.CopyTo(id, 0);
+            id[previousSize] = replicaID;
+            this.tuple = tuple;
+        }
+    }
+}
