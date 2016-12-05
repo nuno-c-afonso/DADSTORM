0120c9503408bd75a91b3d179a14ecce8cffe391
nuno-c-afonso <nuno.c.afonso@ist.utl.pt>
2016-12-05 15:47:45 +0000

    Moved TupleWrapper to the right place

--- DADSTORM/CommonClasses/TupleWrapper.cs
+++ DADSTORM/CommonClasses/TupleWrapper.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Replica {
+namespace CommonClasses {
+    [Serializable]
     public class TupleWrapper {
         private int[] id;
         private string[] tuple;
